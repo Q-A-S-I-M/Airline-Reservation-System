c7e3c9a09a898af23a56f530c42aee534e13f8f3
student <student24@khifast.nu.edu.pk>
2025-04-18 10:29:30 +0500

    Flight updated

--- src/main/java/com/AirlineManagement/Airline_Management_System/Misc/FlightFilter.java
+++ src/main/java/com/AirlineManagement/Airline_Management_System/Misc/FlightFilter.java
@@ -1,1 +1,12 @@
+import java.util.*;
 
+public class FlightFilter{
+    private String fromLocation;
+    private String toLocation;
+    private Date departure;
+    private Date arrival;
+    private int maxRange;
+    private int minRange;
+    private int seats;
+
+}
